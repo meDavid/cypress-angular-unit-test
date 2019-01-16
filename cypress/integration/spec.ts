b60ca2c3f424c9a4a25030980cd1260f9bb6529c
David <D.Kingma@jool.nl>
2019-01-16 10:39:56 +0100

    Include reflection polyfill & zone.js

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,6 +1,9 @@
+// Required for JIT in NG-7
+import 'core-js/es7/reflect';
 import {AppComponent} from '../../src/app/app.component';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import 'zone.js';
 import {mount, TestingModule} from '../../projects/cypress-angular-unit-test/src';
 
 @NgModule({
